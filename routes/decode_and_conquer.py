5668cbef431ba50fa8eb5153311b64552ef43058
takerens <itsamemario2002@gmail.com>
2024-09-28 16:40:01 +0800

    a

--- routes/decode_and_conquer.py
+++ routes/decode_and_conquer.py
@@ -2,39 +2,39 @@ import json
 import logging
 
 from flask import request
+from flask import jsonify
 
 from routes import app
 
 logger = logging.getLogger(__name__)
 
 
-@app.route('/ub5-flags', methods=['POST'])
+@app.route('/ub5-flags', methods=['GET'])
 def evaluate():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
 
     result = {
-  "sanityScroll": {
-    "flag": "UB5{w3lc0m3_70_c7f_N0ttyB01}"
-  },
-  "openAiExploration": {
-    "flag": "FLAG_CONTENT_HERE"
-  },
-  "dictionaryAttack": {
-    "flag": "UB5{FLAG_CONTENT_HERE}",
-    "password": "PASSWORD_HERE"
-  },
-  "pictureSteganography": {
-    "flagOne": "UB5-1{1_am_d3f_n0t_old}",
-    "flagTwo": "UB5-2{FLAG_TWO_CONTENTS_HERE}"
-  },
-  "reverseEngineeringTheDeal": {
-    "flag": "FLAG_CONTENT_HERE",
-    "key": "KEY_HERE"
-  }
-}
+                "sanityScroll": {
+                    "flag": "UB5{w3lc0m3_70_c7f_N0ttyB01}"
+                },
+                "openAiExploration": {
+                    "flag": "FLAG_CONTENT_HERE"
+                },
+                "dictionaryAttack": {
+                    "flag": "UB5{FLAG_CONTENT_HERE}",
+                    "password": "PASSWORD_HERE"
+                },
+                "pictureSteganography": {
+                    "flagOne": "UB5-1{1_am_d3f_n0t_old}",
+                    "flagTwo": "UB5-2{FLAG_TWO_CONTENTS_HERE}"
+                },
+                "reverseEngineeringTheDeal": {
+                    "flag": "FLAG_CONTENT_HERE",
+                    "key": "KEY_HERE"
+                }
+            }
 
-    logging.info("instructions :{}".format(result))
-    return json.dumps(result)
+    return jsonify(result)
 
 
