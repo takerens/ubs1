ae6b21cd816c17a13d39273240570083a0da559f
takerens <itsamemario2002@gmail.com>
2024-09-28 16:31:34 +0800

    a

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
-import routes.efficient_hunter_kazuma
-import routes.dodge_bullet
+# import routes.efficient_hunter_kazuma
+# import routes.dodge_bullet
 import routes.decode_and_conquer
