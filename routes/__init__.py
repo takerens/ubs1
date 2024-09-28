ee4f339d659ff442cec26bff1bc950a90e2f0fe6
takerens <itsamemario2002@gmail.com>
2024-09-29 00:31:55 +0800

    a

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-import routes.efficient_hunter_kazuma
+# import routes.efficient_hunter_kazuma
 # import routes.dodge_bullet
 # import routes.decode_and_conquer
+import routes.the_clumsy_programmer
---
+++ routes/the_clumsy_programmer.py
@@ -0,0 +1,43 @@
+import json
+import logging
+
+from flask import request
+from flask import jsonify
+
+from routes import app
+
+logger = logging.getLogger(__name__)
+
+
+@app.route('/the-clumsy-programmer', methods=['POST'])
+def evaluate():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+
+    result = []
+    for jsonData in data:
+        correctionDict = {}
+        corrections = []
+        dictionary = jsonData["dictionary"]
+        mistypes = jsonData["mistypes"]
+
+        def search(error, dictionary):
+            differences = 0
+            for string in dictionary:
+                differences = 0
+                if (len(error) == len(string)):
+                    for i in range(len(string)):
+                        if string[i] != error[i]:
+                            differences += 1
+                if differences == 1:
+                    return string
+
+        for mistype in mistypes:
+            corrections.append(search(mistype, dictionary))
+            print(mistype)
+        
+        correctionDict["corrections"] = corrections
+        result.append(correctionDict)
+
+        # logging.info("efficiency :{}".format(result))
+    return jsonify(result)
