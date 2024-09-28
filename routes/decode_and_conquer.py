3bb5ffcc0a8b05098a2f0f9f3ad72123fca0bccb
takerens <itsamemario2002@gmail.com>
2024-09-28 17:56:19 +0800

    Update decode_and_conquer.py

--- routes/decode_and_conquer.py
+++ routes/decode_and_conquer.py
@@ -4,37 +4,36 @@ import logging
 from flask import request
 from flask import Response
 
-from routes import app
 
 logger = logging.getLogger(__name__)
 
+app = Flask(__name__)
 
 @app.route('/ub5-flags', methods=['GET'])
 def evaluate():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
 
-    # result = {
-    #             "sanityScroll": {
-    #                 "flag": "UB5{w3lc0m3_70_c7f_N0ttyB01}"
-    #             },
-    #             "openAiExploration": {
-    #                 "flag": "FLAG_CONTENT_HERE"
-    #             },
-    #             "dictionaryAttack": {
-    #                 "flag": "UB5{FLAG_CONTENT_HERE}",
-    #                 "password": "PASSWORD_HERE"
-    #             },
-    #             "pictureSteganography": {
-    #                 "flagOne": "UB5-1{1_am_d3f_n0t_old}",
-    #                 "flagTwo": "UB5-2{FLAG_TWO_CONTENTS_HERE}"
-    #             },
-    #             "reverseEngineeringTheDeal": {
-    #                 "flag": "FLAG_CONTENT_HERE",
-    #                 "key": "KEY_HERE"
-    #             }
-    #         }
-    result = {}
+    result = {
+                "sanityScroll": {
+                    "flag": "UB5{w3lc0m3_70_c7f_N0ttyB01}"
+                },
+                "openAiExploration": {
+                    "flag": "FLAG_CONTENT_HERE"
+                },
+                "dictionaryAttack": {
+                    "flag": "UB5{FLAG_CONTENT_HERE}",
+                    "password": "PASSWORD_HERE"
+                },
+                "pictureSteganography": {
+                    "flagOne": "UB5-1{1_am_d3f_n0t_old}",
+                    "flagTwo": "UB5-2{FLAG_TWO_CONTENTS_HERE}"
+                },
+                "reverseEngineeringTheDeal": {
+                    "flag": "FLAG_CONTENT_HERE",
+                    "key": "KEY_HERE"
+                }
+            }
 
     response = app.response_class(
         response=json.dumps(result),
