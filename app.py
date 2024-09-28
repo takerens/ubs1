7ba2cc13c1a51437060f621a6e6ea2b6fb739d45
takerens <itsamemario2002@gmail.com>
2024-09-28 13:30:38 +0800

    c

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ logger.setLevel(logging.INFO)
 if __name__ == "__main__":
     logging.info("Starting application ...")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('localhost', 8080))
+    sock.bind(('localhost', 8081))
     port = sock.getsockname()[1]
     sock.close()
     app.run(port=port)
